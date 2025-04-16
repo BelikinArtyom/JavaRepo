bc01c1395c0dcb7d927e663a9ccc26c85ad090e6
Artyom Belikin <biloplus@yandex.ru>
2025-04-16 17:14:06 +0300

    Junit5 code example assestion

---
+++ src/test/java/JunitPageSearch.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class JunitPageSearch {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "2560x1440";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void junitsearch (){
+
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $$(".js-wiki-sidebar-toggle-display").last().shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").$(Selectors.byText("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+
+}
