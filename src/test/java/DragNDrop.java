320d9a8575cb790f544dddc517a01c37f0dc649a
Artyom Belikin <biloplus@yandex.ru>
2025-04-21 21:14:57 +0300

    Task5 - Solutions v4

--- src/test/java/DragNDrop.java
+++ src/test/java/DragNDrop.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragNDrop {
@@ -19,9 +20,11 @@ public class DragNDrop {
     void dragnDropactiontest() {
 
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
        // actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform(); Один из вариантов прохождения Drag'n'Drop
         $("#column-a").dragAndDropTo($("#column-b"));
-        $("#column-b").shouldHave(Condition.text("A"));
+        $("#column-b").shouldHave(text("A"));
     }
 
 
