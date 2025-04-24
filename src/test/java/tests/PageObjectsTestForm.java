ba6ac2d6488cc494e18371cf5a87f668c7d6669e
Artyom Belikin <biloplus@yandex.ru>
2025-04-24 12:23:39 +0300

    Task6 - PageObjects 1.2

--- src/test/java/tests/PageObjectsTestForm.java
+++ src/test/java/tests/PageObjectsTestForm.java
@@ -36,31 +36,6 @@ public class PageObjectsTestForm {
 
         registrationPage.checkResult("Label", "value");
 
-
-//        registrationPage.tableCheckResult("Student Name","Паша Техник")
-//                .tableCheckResult("Student Email","xanax@techique.com")
-//                .tableCheckResult("Gender","Male")
-//                .tableCheckResult("Mobile","8800555353")
-//                .tableCheckResult("Date of Birth","24 July,1990")
-//                .tableCheckResult("Mobile","8800555353")
-//                .tableCheckResult("Subjects","Chemistry")
-//                .tableCheckResult("Hobbies","Reading, Sports")
-//                .tableCheckResult("Picture","test_img.jpg")
-//                .tableCheckResult("Address","Улица Пушкина, дом Колотушкина")
-//                .tableCheckResult("State and City","Uttar Pradesh Agra");
-
-//        $("table").shouldHave(text("Student Name"), text("Паша Техник"));
-//        $("table").shouldHave(text("Student Email"), text("xanax@techique.com"));
-//        $("table").shouldHave(text("Gender"), text("Male"));
-//        $("table").shouldHave(text("Mobile"), text("8800555353"));
-//        $("table").shouldHave(text("Date of Birth"), text("24 July,1990"));
-//        $("table").shouldHave(text("Subjects"), text("Chemistry"));
-//        $("table").shouldHave(text("Hobbies"), text("Паша Техник"));
-//        $("table").shouldHave(text("Picture"), text("test_img.jpg"));
-//        $("table").shouldHave(text("Address"), text("Улица Пушкина, дом Колотушкина"));
-//        $("table").shouldHave(text("State and City"), text("Uttar Pradesh Agra"));
-
-
     }
 
 }
