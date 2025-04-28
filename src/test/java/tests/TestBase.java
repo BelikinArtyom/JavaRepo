4d906487ce68fb553192fea773488bebfd4da6d7
Artyom Belikin <biloplus@yandex.ru>
2025-04-28 21:56:37 +0300

    Task6 - PageObjects 2.4

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
     @BeforeAll
     public static void beforeAll() {
         Configuration.browserSize = "2560x1440";
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class TestData {
+
+    public static String firstName = "John",
+                         lastName = "Doe",
+                         email = "qwerty@zxc.com";
+
+
+
+
+
+}
