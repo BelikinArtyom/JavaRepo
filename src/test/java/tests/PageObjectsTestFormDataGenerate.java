52c15558aebaf8a0fae392c6e7c1600c29e1b85d
Artyom Belikin <biloplus@yandex.ru>
2025-04-29 20:05:52 +0300

    Task6 - PageObjects 2.4

--- src/test/java/tests/PageObjectsTestFormDataGenerate.java
+++ src/test/java/tests/PageObjectsTestFormDataGenerate.java
@@ -1,20 +1,36 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static utils.RandomUtils.getRandomEmail;
+import static utils.RandomUtils.getRandomString;
 
 public class PageObjectsTestFormDataGenerate extends TestBase {
 
     @Test
     void successfulRegistrationTest() {
 
-         String firstName = "John",
-                lastName = "Doe",
-                email = "qwerty@zxc.com";
+        String firstName = getRandomString(10);
+        String lastName = getRandomString(10);
+        String userEmail = getRandomEmail();
 
+
+//        Faker faker = new Faker (new Locale("sv"));
+//
+//        String name = faker.name().fullName();
+//        String firstName = faker.name().firstName();
+//        String lastName = faker.name().lastName();
+//        String userEmail = faker.internet().emailAddress();
+//        String streetAddress = faker.address().streetAddress();
+
+
+//         String firstName = "John",
+//                lastName = "Doe",
+//                email = "qwerty@zxc.com";
 
 
         open("/automation-practice-form");
@@ -24,7 +40,7 @@ public class PageObjectsTestFormDataGenerate extends TestBase {
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
+        $("#userEmail").setValue(userEmail);
         $("#genterWrapper").$(byText("Other")).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
@@ -44,6 +60,9 @@ public class PageObjectsTestFormDataGenerate extends TestBase {
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(text(firstName), text(lastName),
-                text(email), text("1234567890"));
+                text(userEmail), text("1234567890"));
+
+        sleep (90000);
     }
+
 }
