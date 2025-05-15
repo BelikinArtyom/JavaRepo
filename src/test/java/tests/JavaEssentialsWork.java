39676d30d3c261126ed3b1876b986244226a862f
Artyom Belikin <biloplus@yandex.ru>
2025-05-15 14:43:58 +0300

    javaEssentials/2

--- src/test/java/tests/JavaEssentialsWork.java
+++ src/test/java/tests/JavaEssentialsWork.java
@@ -9,16 +9,18 @@ public class JavaEssentialsWork {
         work.JavaOperations();
     }
 
-      private static void  JavaOperations(){
+      private static void  JavaOperations() {
 
-        String conditionUsed = "Used";
-        String conditionNew = "New";
-        int basePrice = 4000;
-        int tax = 150;
-        double delivery = 99.99;
-        int customFee = 2;
-        int discount = 10;
-        double totalPrice = basePrice + tax + delivery * customFee - discount;
+          //операции над данными
+
+          String conditionUsed = "Used";
+          String conditionNew = "New";
+          int basePrice = 4000;
+          int tax = 150;
+          double delivery = 99.99;
+          int customFee = 2;
+          int discount = 10;
+          double totalPrice = basePrice + tax + delivery * customFee - discount;
 //
 //          Boolean tradeInAvailable = conditionNew != "New" && conditionUsed == "Used";
 //            System.out.println(tradeInAvailable);
@@ -27,23 +29,39 @@ public class JavaEssentialsWork {
           Boolean tradeInAvailable = !condition.contains("New") && condition.contains("Used");
           System.out.println(tradeInAvailable);
 
-         System.out.println("Ваша цена: " +totalPrice);
+          System.out.println("Ваша цена за устройство: " + totalPrice);
 
 
-         boolean saleApproved = true;
-         boolean saleRejected = false;
+          boolean saleApproved = true;
+          boolean saleRejected = false;
 
-         int age = 18;
-         int fine = 5000;
-         int alcogholSaleAge = 21;
+          int age = 18;
+          int fine = 5000;
+          int alcogholSaleAge = 21;
 
-         if (age >= alcogholSaleAge) {
-             System.out.println(saleApproved);
-         } else if (age <= alcogholSaleAge && saleApproved) {
-             System.out.println("Вам штрафик" + ' ' + fine);
-         } else {
-             System.out.println(saleRejected);
-         }
-    }
-}
+          if (age >= alcogholSaleAge) {
+              System.out.println(saleApproved);
+          } else if (age < alcogholSaleAge && saleApproved) {
+              System.out.println("Вам штрафик" + ' ' + fine + ' ' + "рубликов");
+          } else {
+              System.out.println(saleRejected);
+          }
+
+
+          // блок с переполнениями
+              double a = 1e308;
+              double b = 10;
+              double result = a * b; // переполнение
+
+              System.out.println(result); // положительный результат переполнения
+
+              double negativeOverflow = -a * b;
+              System.out.println(negativeOverflow); // отрицательный результат переполнения
+          }
+      }
+
+
+
+
+
 
