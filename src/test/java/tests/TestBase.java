5784fd093ff4440ac60aa7400f7e7ce4afe16b22
Artyom Belikin <biloplus@yandex.ru>
2025-05-28 18:20:34 +0300

    Homework Update

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,33 +13,43 @@ import java.util.UUID;
 
 public class TestBase {
 
-    private static final String SELENOID_URL = System.getProperty("selenoid.url");
-    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");
-    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
-    private static final String BROWSER = System.getProperty("browser", "chrome");
-    private static final String BROWSER_SIZE = System.getProperty("browser.size", "1920x1080");
+//    private static final String SELENOID_URL = System.getProperty("selenoid.url");
+//    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");
+//    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
+
 
 
     @BeforeAll
     public static void beforeAll() {
-        System.out.println("SELENOID_URL = " + SELENOID_URL);
-        System.out.println("SELENOID_LOGIN = " + SELENOID_LOGIN);
-        System.out.println("SELENOID_PASSWORD = " + SELENOID_PASSWORD);
-        System.out.println("BROWSER = " + BROWSER);
-        System.out.println("BROWSER_SIZE = " + BROWSER_SIZE);
+
+        String selenoidHost = System.getProperty("selenoid_host", "selenoid.autotests.cloud");
+        String selenoidLogin = System.getProperty("selenoid_login", "user1");
+        String selenoidPassword = System.getProperty("selenoid_password", "1234");
+        String browserVersion = System.getProperty("browserVersion", "127.0");
+        String BROWSER = System.getProperty("browser", "chrome");
+        String BROWSER_SIZE = System.getProperty("browser.size", "1920x1080");
+//        System.out.println("SELENOID_URL = " + SELENOID_URL);
+//        System.out.println("SELENOID_LOGIN = " + SELENOID_LOGIN);
+//        System.out.println("SELENOID_PASSWORD = " + SELENOID_PASSWORD);
+//        System.out.println("BROWSER = " + BROWSER);
+//        System.out.println("BROWSER_SIZE = " + BROWSER_SIZE);
 
         Configuration.browser = BROWSER;
+        Configuration.browserVersion = browserVersion;
         Configuration.browserSize = BROWSER_SIZE;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = true;
         Configuration.holdBrowserOpen = false;
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
+                selenoidLogin,
+                selenoidPassword,
+                selenoidHost);
 
-        if (SELENOID_URL != null && SELENOID_LOGIN != null && SELENOID_PASSWORD != null) {
-            Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
-        }
 
+
+
+//      Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
@@ -47,14 +57,6 @@ public class TestBase {
                 "name", "Test: " + UUID.randomUUID()
         ));
         Configuration.browserCapabilities = capabilities;
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
-
-    @Test
-    public void sele() {
-        System.out.println("SELENOID_URL = " + SELENOID_URL);
-        System.out.println("SELENOID_LOGIN = " + SELENOID_LOGIN);
-        System.out.println("SELENOID_PASSWORD = " + SELENOID_PASSWORD);
-        }
-    }
+}
