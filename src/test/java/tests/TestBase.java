a0fab901b21645cacb906b327d98a41daa9d4941
Artyom Belikin <biloplus@yandex.ru>
2025-05-28 17:00:34 +0300

    Homework Update

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -8,8 +8,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class TestBase {
+
+    private static final String SELENOID_URL = System.getProperty("selenoid.url");
+    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");;
+    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
+
 
     @BeforeAll
     public static void beforeAll() {
@@ -17,8 +23,16 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = false;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true,
+                "name", "Test: " + UUID.randomUUID()
+        ));
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
+        Configuration.browserCapabilities = capabilities;
+        Configuration.holdBrowserOpen = false;
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
