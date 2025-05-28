866992c5ae50e313d2adbdea5c961d9d3a5a53ee
Artyom Belikin <biloplus@yandex.ru>
2025-05-28 16:55:01 +0300

    Homework Update

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,7 +17,9 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = false;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
