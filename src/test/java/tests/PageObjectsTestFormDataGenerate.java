477e086de421d367cc8642fcacd7ed8de16c1e5a
Artyom Belikin <biloplus@yandex.ru>
2025-07-02 16:44:08 +0300

    Homework Update

--- src/test/java/tests/PageObjectsTestFormDataGenerate.java
+++ src/test/java/tests/PageObjectsTestFormDataGenerate.java
@@ -83,39 +83,4 @@ public class PageObjectsTestFormDataGenerate extends TestBase {
 
 
     }
-
-//        TestData data = new TestData();
-//        data.generateRandomHobbies();
-//        data.generateRandomPictureName();
-//        registrationPage.openPage();
-//        registrationPage.setTestData(data);
-//        registrationPage
-//                .setFirstName(firstName)
-//                .setLastName(lastName)
-//                .setEmail(email)
-//                .selectGender(TestData.gender);
-//        registrationPage.setPhoneNumber(faker.number().digits(6));
-//        registrationPage.setRandomBirthDateWithFaker();
-//        registrationPage.selectSubjects(Collections.singletonList(singleSubject));
-//        registrationPage.selectHobbies(data.selectedHobbySelectors);
-//        registrationPage.uploadPicture(data.getUploadedPictureName());
-//        registrationPage.setAdress(adress);
-//        testData.setRandomStateAndCity();
-//        registrationPage
-//                .submitButton();
-//        registrationPage
-//                .checkTableResult("Student Name", firstName + " " + lastName)
-//                .checkTableResult("Student Email", email)
-//                .checkTableResult("Gender", TestData.gender)
-//                .checkTableResult("Mobile", registrationPage.getEnteredPhone());
-//        registrationPage.checkBirthDateInResult(resultTable);
-//        registrationPage
-//                .checkSubjectsInResult();
-//        registrationPage
-//                .checkHobbiesInResult();
-//        registrationPage
-//                .checkStateAndCityInResult(testData.selectedStateAndCity);
-//        registrationPage.checkTableResult("Address", adress);
-//
-
     }
